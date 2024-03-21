ccc158bfaa98d6efc8d305a2700999dcefcaeadb
SealAnia <koshkoanna916@gmail.com>
2024-03-21 15:43:37 +0000

    packages and dependencies added

---
+++ src/test/java/com/example/online_notepad/OnlineNotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_notepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineNotepadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
